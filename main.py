a1bf0d239094b192a6a6f017cf6bf03bd87cad59
Ryabov Andrey <araibov31242@gmail.com>
2025-08-12 12:52:23 +0000

    add cors

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 from app.api.task import router as task_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="api")
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(task_router)
+
 
 @app.get("/")
 async def root():
