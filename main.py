00a5714e53198b3de09cacf6120e0671cb39855d
Ryabov Andrey <araibov31242@gmail.com>
2025-08-13 11:51:48 +0000

    allow all hosts

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(title="api")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
